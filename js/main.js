9af1555cab11e35046ce3ff7193135eae742bd55
Jayson Tarrosa <jaysontarrosa@Jaysons-MacBook-Pro.local>
2023-07-29 13:24:10 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+    e.preventDefault();
+    removeBorder();
+    removeShow();
+    console.log('click');
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
+    console.log('id');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
